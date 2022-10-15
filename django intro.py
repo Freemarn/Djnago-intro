f7d12ff83d3490f6c0e3fd82eebee62d1fbdd91a
Immanuel Alika Prosper <101982494+Freemarn@users.noreply.github.com>
2022-10-15 22:33:50 +0100

    introduction to django

---
+++ django intro.py
@@ -0,0 +1,20 @@
+# django is a high level python frame work that makes application development easy
+
+#1 django installation---- py -m pip install Django
+
+#2 create a virtual environment
+# py -m venv [project name]
+
+#3 Activate virtual environment
+#project\scripts\activate.bat
+
+#4 check django version
+#django-admin --version
+
+#5 django has a command line that can help you create an app inside your project
+#django-admin startproject [app name]
+
+#6 create multiple apps in Django using the command line Below
+#python manage.py startapp [App name]
+
+
